b71fdbb609b8f8a5629ceae6c5ac1a87f7c94f15
ShinMyeongJi <shinmj1207@gmail.com>
2020-07-22 17:57:16 +0900

    코드 수정

--- Bertrand/Betrand.cpp
+++ Bertrand/Betrand.cpp
@@ -2,43 +2,34 @@
 #include <queue>
 #include <vector>
 using namespace std;
-#define MAX 123456
 /**
- * BOJ 4948 (시간복잡도 : N²)
+ * BOJ 4948
  */
 int main(void){
-    while(true) {
+    while(true){
         int n;
         int cnt = 0;
         cin >> n;
 
         if(n == 0) break;
-        
+
         bool notPrimes[2*n] = {};
 
-        notPrimes[0] = true;
-        notPrimes[1] = true;
-        
-        for(int i = 2; i <= 2*n; i++){
+        notPrimes[0] = notPrimes[1] = true;
+
+        for(int i = 2; i < 2*n; i++){
             if(notPrimes[i]) continue;
-            for(int j = i*2; j < 2*n; j+=i){
+            for(int j = i * 2; j < 2*n; j+=i){
                 notPrimes[j] = true;
             }
         }
-    
-        if(n != 1) {
-            for(int i = n+1; i < 2*n; i++){
-                cnt += !notPrimes[i] ? 1 : 0;
-            }
-        }else{
-            for(int i = n+1; i <= 2*n; i++){
-                cnt += !notPrimes[i] ? 1 : 0;
-            }
-        }
-        
 
+
+        for(int i = n + 1; i < 2*n; i++){
+            if(!notPrimes[i]) cnt++;
+        }
+        if(n == 1) cnt++;
         cout << cnt << endl;
-        
     }
 }
 
