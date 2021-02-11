66db900b1adf498a1b1b2148e0246de8091e3740
ShinMyeongJi <shinmj1207@gmail.com>
2021-02-11 23:58:09 +0900

    210211 알고리즘

--- BOJ1018/src/Main.java
+++ BOJ1018/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        int N, Y;
 
+        Scanner kb = new Scanner(System.in);
+
+        N = kb.nextInt();
+        Y = kb.nextInt();
+
+        char[][] board = new char[N][Y];
+
+        for(int i = 0; i < N; i++){
+            String str = kb.next();
+            for(int j = 0; j < Y; j++) {
+                board[i][j] = str.charAt(j);
+            }
+        }
+
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < Y; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
