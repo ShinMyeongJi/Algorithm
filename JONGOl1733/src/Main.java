e25c20b368ffa0225814705ebc95157ee76ecdc7
ShinMyeongJi <shinmj1207@gmail.com>
2021-03-23 23:05:30 +0900

    코드 수정

--- JONGOl1733/src/Main.java
+++ JONGOl1733/src/Main.java
@@ -1,12 +1,11 @@
-import com.sun.javaws.IconUtil;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
+/**
+ * JONGOL 1733
+ */
 
 public class Main {
     public static final int SIZE = 19;
@@ -24,121 +23,114 @@ public class Main {
 
         }
 
+
         for(int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 if(board[i][j] == 1 || board[i][j] == 2) {
 
-                    checkOmok(i , j);
+                    if(checkOmok(i , j)) return;
+
                 }
             }
         }
+        System.out.println(0);
     }
 
-    public static void checkOmok(int i, int j) {
+    public static boolean checkOmok(int i, int j) {
         int num = board[i][j];
         int ori_i = i, ori_j = j;
-        int cnt = 0;
+        int cnt = 1;
+
         //오른쪽
-        for(int li = 0; li < 5; li++, j++) {
-            if(cnt == 4) {
-                if (ori_i != 0 || ori_j != 0 || ori_i != 18 || ori_j != 18) {
-                    if(board[ori_i][ori_j-1] == num || board[i][j+1] == num){
-                        cnt = 0;
-                        break;
-                    }else{
-                        System.out.println(num);
-                        System.out.println((ori_i+1) + " " + (ori_j+1));
-                        return;
-                    }
-                }
-            }
-            if(num != board[i][j+1]) {
-                cnt = 0;
+        for(int li = 1; li <= 4; li++) {
+            if(i > 18 || i < 1 || j + li >= 18 || j + li <= 1) break;
+            if(num == board[i][j+li]) {
+
+                cnt++;
+            }else{
+                cnt = 1;
                 break;
             }
 
-            cnt++;
+            if(cnt == 5){
+                if(num != board[i][j + li + 1] && num != board[i][j - 1]){
+
+                    System.out.println(num);
+                    System.out.println((ori_i+1) + " " + (ori_j+1));
+                    return true;
+                }
+            }
         }
 
 
         //오른쪽 아래
-        for(int li = 0; li < 5; li++, i++, j++) {
-            if(cnt == 4) {
-                if (ori_i != 0 || ori_j != 0 || ori_i != 18 || ori_j != 18) {
-                    if(board[ori_i-1][ori_j-1] == num || board[i+1][j+1] == num){
-                        cnt = 0;
-                        break;
-                    }else{
-                        System.out.println(num);
-                        System.out.println((ori_i+1) + " " + (ori_j+1));
-                        cnt = 0;
-                        return;
-                    }
-                }
-            }
-            if(num != board[i+1][j+1]) {
+        for(int li = 1; li <= 4; li++) {
+            if(i + li + 1 >= 18 || i + li +1 <= 1 || j + li >= 18 || j + li <= 1) break;
+            if(num == board[i+li][j+li]) {
 
-                cnt = 0;
+                cnt++;
+            }else{
+                cnt = 1;
                 break;
             }
-            cnt++;
+
+            if(cnt == 5){
+                if(num != board[i + li + 1][j + li + 1] && num != board[i - 1][j - 1]){
+
+                    System.out.println(num);
+                    System.out.println((ori_i+1) + " " + (ori_j+1));
+                    return true;
+                }
+            }
         }
 
 
         //오른쪽 위
-        for(int li = 0; li < 5; li++, i--, j++) {
-
-            if(cnt == 4) {
-                System.out.println(i + "," + j);
-            }
-
-            if(num != board[i-1][j+1]){
-                cnt = 0;
+        for(int li = 1; li <= 4; li++) {
+            if(i - li >= 18 || i - li <= 1 || j + li >= 18 || j + li <= 1) break;
+            if(num == board[i-li][j+li]) {
+                cnt++;
+            }else{
+                cnt = 1;
                 break;
             }
 
-            cnt++;
-            if(cnt == 4) {
-                if (ori_i != 0 || ori_j != 0 || ori_i != 18 || ori_j != 18) {
+            if(cnt == 5){
+                if(num != board[i - li - 1][j + li + 1] && num != board[i + 1][j - 1]){
 
-                    if(board[ori_i+1][ori_j-1] == num || board[i-1][j+1] == num){
-                        cnt = 0;
-                        break;
-                    }else{
-                        System.out.println(num);
-                        System.out.println((ori_i+1) + " " + (ori_j+1));
-                        return;
-                    }
+                    System.out.println(num);
+                    System.out.println((ori_i+1) + " " + (ori_j+1));
+                    return true;
                 }
             }
-            if(num != board[i-1][j+1]) {
-                cnt = 0;
-                break;
-            }
-
-            cnt++;
         }
+
+
 
         //아래
-        for(int li = 0; li < 5; li++, i++) {
-            if(cnt == 4) {
-                if (ori_i != 0 || ori_j != 0 || ori_i != 18 || ori_j != 18) {
-                    if(board[ori_i-1][ori_j] == num || board[i+1][j] == num){
-                        cnt = 0;
-                        break;
-                    }else{
-                        System.out.println(num);
-                        System.out.println((ori_i+1) + " " + (ori_j+1));
-                        cnt = 0;
-                        return;
-                    }
-                }
-            }
-            if(num != board[i+1][j]) {
-                cnt = 0;
+        for(int li = 1; li <= 4; li++) {
+            if(i+li >= 18 || i + li <= 1 || j > 18 || j < 1) break;
+            if(num == board[i+li][j]) {
+
+                cnt++;
+            }else{
+                cnt = 1;
                 break;
             }
-            cnt++;
+
+            if(cnt == 5){
+                if(num != board[i + li + 1][j] && num != board[i - 1][j]){
+
+                    System.out.println(num);
+                    System.out.println((ori_i+1) + " " + (ori_j+1));
+                    return true;
+
+                }
+            }
         }
+
+        return false;
+
+
     }
 }
