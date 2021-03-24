a9f52ea6de8824a2c1792200f81539b061193cff
ShinMyeongJi <shinmj1207@gmail.com>
2021-03-24 23:30:54 +0900

    210324 알고리즘

--- JONGOL1031/src/Main.java
+++ JONGOL1031/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static int[][] board = new int[SIZE][SIZE];
     public static int[][] calledNums = new int[SIZE][SIZE];
     public static boolean[][] isVisited = new boolean[SIZE][SIZE];
+    public static int called = 0;
     public static void main(String[] args) throws IOException {
 
         BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
@@ -21,30 +22,33 @@ public class Main {
         }
 
         for(int i = 0; i < SIZE; i++) {
-           st = new StringTokenizer(bw.readLine());
+            st = new StringTokenizer(bw.readLine());
             for(int j = 0; j < SIZE; j++) {
                 calledNums[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+
         for(int i = 0; i < SIZE; i++) {
             for(int j = 0; j < SIZE; j++) {
-                checkBingo(calledNums[i][j]);
+                called++;
+                if(checkBingo(calledNums[i][j]) && called >= 12){
+                    System.out.println(called);
+                    return;
+                }
             }
         }
 
 
     }
 
-    public static void checkBingo(int calledNum) {
-        int cur_i = 0;
-        int cur_j = 0;
+    public static boolean checkBingo(int calledNum) {
+        int bingo = 0;
+
         for(int i = 0; i < SIZE; i++ ){
             for(int j = 0; j < SIZE; j++) {
                 if(calledNum == board[i][j]) {
                     isVisited[i][j] = true;
-                    cur_i = i;
-                    cur_j = j;
                     break;
                 }
             }
@@ -52,46 +56,67 @@ public class Main {
 
 
         //현재 방문한 기준으로 가로, 세로, 좌우 대각선 빙고 확인
+        bingo = checkHor() + checkVer() + checkDiagPlus() + checkDiagMinus();
 
-        //가로
-        checkHor(cur_i, cur_j);
-        //세로
-        checkVer(cur_i, cur_j);
-        //기울기 양의 대각선
-        checkDiagPlus(cur_i, cur_j);
-        //기울기 음의 대각선
-        checkDiagMinus(cur_i, cur_j);
+        if(bingo >= 3) return true;
 
 
+        return false;
     }
 
-    public static void checkHor(int cur_i, int cur_j) {
-        int cnt = 0;
-        for(int j = 0; j < cur_j; j++) {
-            if(isVisited[cur_i][j])
-            {
-                cnt++;
+    //가로
+    public static int checkHor() {
+        int numBingo = 0;
+
+        for(int i = 0; i < SIZE; i++) {
+            int cnt = 0;
+            for(int j = 0; j < SIZE; j++) {
+                if(isVisited[i][j])  ++cnt;
+                if(cnt == 5) ++numBingo;
             }
         }
 
-        for(int j = SIZE -1; j > cur_j; j--) {
-            if(isVisited[cur_i][j]){
-                cnt++;
+        return numBingo;
+    }
+
+    //세로
+    public static int checkVer(){
+        int numBingo = 0;
+
+        for(int i = 0; i < SIZE; i++) {
+            int cnt = 0;
+            for(int j = 0; j < SIZE; j++) {
+                if(isVisited[j][i])  ++cnt;
+                if(cnt == 5) ++numBingo;
             }
         }
 
-        System.out.print(cnt);
+        return numBingo;
+    }
+
+    //기울기 : 양의 정수, 대각선
+    public static int checkDiagPlus(){
+        int numBingo = 0;
+        int j = SIZE - 1;
+        for(int i = 0; i < SIZE; i++) {
+            if(isVisited[i][j--]) ++numBingo;
+            if(numBingo == 5) return 1;
+        }
+
+
+        return 0;
 
     }
 
-    public static void checkVer(int cur_i, int cur_j){
+    //기울기 : 음의 정수, 대각선
+    public static int checkDiagMinus(){
+        int numBingo = 0;
 
-    }
+        for(int i = 0; i < SIZE; i++) {
+            if(isVisited[i][i]) ++numBingo;
+            if(numBingo == 5) return 1;
+        }
 
-    public static void checkDiagPlus(int cur_i, int cur_j){
-
-    }
-    public static void checkDiagMinus(int cur_i, int cur_j){
-
+        return 0;
     }
 }
