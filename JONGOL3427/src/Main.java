218ddbaebb6d4642bf8c106a6678341a7b03c98f
ShinMyeongJi <shinmj1207@gmail.com>
2021-03-30 22:04:55 +0900

    210330 알고리즘

--- JONGOL3427/src/Main.java
+++ JONGOL3427/src/Main.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    public static boolean statSwitch = false;
+    public static int cnt = 0;
+    public static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         int N = 0;
 
@@ -11,15 +15,39 @@ public class Main {
 
         int[] balls = new int[N];
         String strBalls = br.readLine();
+
+
+        //R를 왼쪽
         for(int i = 0; i < N; i++) {
-            balls[i] = strBalls.charAt(i);
+            if(strBalls.charAt(i) == 'B') statSwitch = true;
+            if(statSwitch && strBalls.charAt(i) == 'R') cnt++;
         }
+        reset();
 
+        for(int i = N - 1; i >= 0; i--) {
+            if(strBalls.charAt(i) == 'B') statSwitch = true;
+            if(statSwitch && strBalls.charAt(i) == 'R') cnt++;
+        }
+        reset();
 
         for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-
-            }
+            if(strBalls.charAt(i) == 'R') statSwitch = true;
+            if(statSwitch && strBalls.charAt(i) == 'B') cnt++;
         }
+        reset();
+
+        for(int i = N - 1; i >= 0; i--) {
+            if(strBalls.charAt(i) == 'R') statSwitch = true;
+            if(statSwitch && strBalls.charAt(i) == 'B') cnt++;
+        }
+        reset();
+
+        System.out.println(min);
+    }
+
+    public static void reset() {
+        statSwitch = false;
+        if(cnt < min) min = cnt;
+        cnt = 0;
     }
 }
