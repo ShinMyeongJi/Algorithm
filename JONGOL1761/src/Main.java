c6a53a5b467f315e295fba2b1faddc32d38f3634
ShinMyeongJi <shinmj1207@gmail.com>
2021-03-31 21:36:42 +0900

    210331 알고리즘

--- JONGOL1761/src/Main.java
+++ JONGOL1761/src/Main.java
@@ -15,6 +15,12 @@ public class Main {
         int[] curTempNum = new int[3]; // 현재 임의의 세자리 숫자
         int[] inputNums = new int[3]; // 입력들어온 세자리 숫자
 
+        int curStrike = 0;
+        int curBalls = 0;
+
+        int cnt = 0;
+        int min = 0;
+
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             random[i] = st.nextToken().trim();
@@ -25,7 +31,7 @@ public class Main {
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 for(int k = 0; k < 9; k++) {
-                    if(i != j && j != k) {
+                    if(i != j && j != k && i != k) {
                         curTempNum[0] = i; curTempNum[1] = j;curTempNum[2] = k;
                         for(int l = 0; l < N; l++) {
                             inputNums[0] = Integer.parseInt(String.valueOf(random[l].charAt(0)));
@@ -34,14 +40,23 @@ public class Main {
 
                             for(int q = 0; q < 3; q++) {
                                 for(int w = 0; w < 3; w++) {
-
+                                    if(curTempNum[q] == inputNums[w]) {
+                                        if(q == w) curStrike++;
+                                        else curBalls++;
+                                    }
                                 }
                             }
+                            if(curStrike == strike[l] && curBalls == balls[l]) cnt++;
+                            curStrike = 0;
+                            curBalls = 0;
                         }
+                        if(N == cnt) min ++;
+                        cnt = 0;
                     }
                 }
             }
         }
+        System.out.println(min);
 
 
     }
