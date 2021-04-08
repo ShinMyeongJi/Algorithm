be219aaf1f60f36b0bca88b3840f36e805181d94
ShinMyeongJi <shinmj1207@gmail.com>
2021-04-08 22:29:34 +0900

    210406 알고리즘

--- JONGOL1221/src/Main.java
+++ JONGOL1221/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,20 +10,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        String[] chars = new String[N];
+        char[] inputs = new char[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < N; i++) {
-            chars[i] = st.nextToken();
+            String temp = st.nextToken();
+            inputs[i] = temp.charAt(0);
         }
 
-        for(int i = 0; i < N; i++) {
-            switch (chars[i].charAt(0)) {
-                case '+' : {
+        Stack stack = new Stack();
 
+        for(int i = 0; i < N; i++) {
+            if(inputs[i] >= '0' && inputs[i] <= '9') {
+                stack.push(Integer.parseInt(String.valueOf(inputs[i])));
+            }else{
+                int a = (int) stack.pop();
+                int b = (int) stack.pop();
+                if(inputs[i] == '+') {
+                    stack.push(a+b);
+                }else if(inputs[i] == '/') {
+                    stack.push(b/a);
+                }else if(inputs[i] == '*') {
+                    stack.push(a*b);
+                }else if(inputs[i] == '-') {
+                    stack.push(b-a);
                 }
             }
         }
 
+        System.out.println(stack.pop());
     }
 }
