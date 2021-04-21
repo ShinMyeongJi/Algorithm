c1227cfaca45022e1977092f8e5ad25f00127ae5
ShinMyeongJi <shinmj1207@gmail.com>
2021-04-22 00:12:02 +0900

    210421 알고리즘

--- JONGOL1175/src/Main.java
+++ JONGOL1175/src/Main.java
@@ -1,26 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N = 0, M = 0;
+    static int N, M;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 횟수
+        M = Integer.parseInt(st.nextToken()); // 합
 
-        N = Integer.parseInt(br.readLine()); // 횟수
-        M = Integer.parseInt(br.readLine()); // 합
-
-        print(0, 0);
+        arr = new int[10];
+        print(1);
     }
 
-    public static void print(int sum, int cnt) { ;
-        if(sum == M && cnt == N) {
-
+    public static void print(int step) {
+        if(step > N) {
+            if(M == sum()) {
+                for(int i = 1; i <=N; i++) {
+                    System.out.print(arr[i] + " ");
+                }
+                System.out.println();
+            }
+            return;
         }
 
-        for(int i = 1; i <= 6; i++) {
-            print(sum + 1, cnt + 1);
+        for(int i = 1; i <=6; i++) {
+            arr[step] = i;
+            print(step + 1);
         }
+
     }
+
+    public static int sum() {
+        int sum = 0;
+        for(int i = 1; i <= N; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+
 }
