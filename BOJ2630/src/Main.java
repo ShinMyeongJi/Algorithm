503e449dd328e1a48c5ba4ae0e4de26cd678129d
ShinMyeongJi <shinmj1207@gmail.com>
2021-04-27 23:28:23 +0900

    210427 알고리즘

--- BOJ2630/src/Main.java
+++ BOJ2630/src/Main.java
@@ -1,23 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        int N = 0;
-        int[][] board = new int[N][N];
 
+    static int[][] board;
+    public static void main(String[] args) throws IOException {
+
+        int N = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        
+        board = new int[N][N];
+
+
         
         for(int i = 0; i < N; i++) {
-            for(int j = 0; i < N; i++) {
-
-                StringTokenizer st = new StringTokenizer(br.readLine());
-
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < N; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        split(0, 0, N);
+
+    }
+
+    public static void split(int x, int y, int N) {
+        int cnt = 0;
+        for(int i = x; i < x+N; i++) {
+            for(int j = y; j < y+N; j++) {
+                if(board[i][j] == 1) cnt++;
             }
         }
     }
