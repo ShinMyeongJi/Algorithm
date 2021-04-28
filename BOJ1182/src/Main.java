694dba6b8b89e69c3a29f912c4bda595b40bc953
ShinMyeongJi <shinmj1207@gmail.com>
2021-04-28 16:26:01 +0900

    210428 알고리즘

--- BOJ1182/src/Main.java
+++ BOJ1182/src/Main.java
@@ -5,19 +5,33 @@ import java.util.StringTokenizer;
 
 public class Main {
     private static int[] arr;
+    private static int N, S = 0;
     private static int cnt = 0;
     public static void main(String[] args) throws IOException {
-        int N = 0;
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
         arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for(int i  = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        sum(0, 0);
+        System.out.println(cnt);
+    }
 
+    public static void sum(int idx, int sum) {
+        if(idx >= N) return;
+        sum += arr[idx];
+
+        if(sum == S) cnt++;
+
+        sum(idx + 1, sum - arr[idx]);
+        sum(idx + 1, sum);
     }
 
 
