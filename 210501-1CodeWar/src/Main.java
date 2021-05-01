7c80d110ae30d784e48942641797fda5d6fe5e36
ShinMyeongJi <shinmj1207@gmail.com>
2021-05-01 22:24:52 +0900

    210501 알고리즘

---
+++ 210501-1CodeWar/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 210501
+ * Code War - Integers: Recreation One
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(listSquared(1, 250));
+
+    }
+
+    public static String listSquared(long m, long n) {
+        //
+        for(long i = m; i < n; i++) {
+            List<Long> divisors = new ArrayList<>();
+            for(long j = 0; j * j < i; j++) {
+                if(i % j == 0) divisors.add(j);
+            }
+        }
+    }
+}
