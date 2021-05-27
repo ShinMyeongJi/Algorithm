fc9a3544ec38340f261ffae06507e3d31f40d1af
ShinMyeongJi <shinmj1207@gmail.com>
2021-05-27 23:50:09 +0900

    210527 알고리즘

---
+++ 210527CodeWars/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(workOnStrings("abc","cde"));
+    }
+
+    public static String workOnStrings(String a, String b) {
+        for(int i = 0; i < a.length(); i++) {
+            if(b.contains(a.charAt(i) + "")) {
+
+            }
+        }
+
+        return "";
+    }
+}
