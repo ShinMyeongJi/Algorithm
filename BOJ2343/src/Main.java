41670a43aac7c9a53f2ba2ff1d05bb9c2d110a56
ShinMyeongJi <shinmj1207@gmail.com>
2021-06-18 23:39:14 +0900

    210618 알고리즘

---
+++ BOJ2343/src/Main.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+/**
+ * 이진탐색의 경 무엇을 탐색하는지를 생각하고 이를 탐색 대상으로 계산할 수 있게 식을 유도하는 것이 옳은 해법이다.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int N, M = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int low = arr[N-1];
+        int high = IntStream.of(arr).sum();
+
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            int cnt = 0, sum = 0;
+            for(int i = 0; i < N; i++) {
+
+                if(sum + arr[i] > mid) {
+                    cnt++;
+                    sum = 0;
+                }
+                sum += arr[i];
+            }
+
+            if(sum != 0) cnt++;
+            if(cnt <= M) {
+                high = mid - 1;
+
+            }else{
+                low = mid + 1;
+            }
+
+        }
+
+        System.out.println(low);
+    }
+}
