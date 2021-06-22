4e5f58c072c3e1ca3b0b08853b444f10afadf864
ShinMyeongJi <shinmj1207@gmail.com>
2021-06-22 18:59:41 +0900

    210622 알고리즘

---
+++ 210622CodeWars/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(pigIt("Pig latin is cool"));
+    }
+
+    public static String pigIt(String str) {
+        String ay = "ay";
+        StringBuilder sb;
+        String[] arr = str.split(" ");
+
+        for(int i = 0; i < arr.length; i++) {
+            char s = arr[i].charAt(0);
+            sb = new StringBuilder(arr[i].substring(1));
+            arr[i] = sb.append(s).append(ay).toString();
+        }
+
+        return String.join(" ", arr);
+    }
+}
