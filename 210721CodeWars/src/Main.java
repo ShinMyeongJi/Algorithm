5a055cf0d13b7282c9c9a8b4cd7ae21bf7377658
ShinMyeongJi <shinmj1207@gmail.com>
2021-07-21 23:31:42 +0900

    210721 알고리즘

---
+++ 210721CodeWars/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 210721 CodeWars - Address Book by State
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        String ad0="John Daggett, 341 King Road, Plymouth MA\n" +
+                "Alice Ford, 22 East Broadway, Richmond VA\n" +
+                "Orville Thomas, 11345 Oak Bridge Road, Tulsa OK\n" +
+                "Terry Kalkas, 402 Lans Road, Beaver Falls PA\n" +
+                "Eric Adams, 20 Post Road, Sudbury MA\n" +
+                "Hubert Sims, 328A Brook Road, Roanoke VA\n" +
+                "Amy Wilde, 334 Bayshore Pkwy, Mountain View CA\n" +
+                "Sal Carpenter, 73 6th Street, Boston MA";
+
+        System.out.println(byState(ad0));
+    }
+
+    public static String byState(String str) {
+        // your code
+
+
+        Map<String, String> loc = new HashMap(){{
+           put("AZ", "Arizona");
+           put("CA", "California");
+           put("", "");
+        }};
+
+        String[] items = str.split("\n");
+
+
+
+        return "";
+    }
+}
