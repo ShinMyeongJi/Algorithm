4d60a905ab50cab3d6d5238fd50701f860aedf56
ShinMyeongJi <shinmj1207@gmail.com>
2021-07-27 21:57:42 +0900

    stream 코드 보완..

--- 210727CodeWars/src/Main.java
+++ 210727CodeWars/src/Main.java
@@ -20,5 +20,12 @@ public class Main {
         return multiples.stream()
                 .distinct()
                 .mapToInt(i -> i).sum();
+
+
+        /**
+         * return IntStream.range(0, number)
+     *                     .filter(n -> (n % 3 == 0) || (n % 5 == 0))
+     *                     .sum();
+         */
     }
 }
