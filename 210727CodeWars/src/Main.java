315050d9a00b69d3cc0c1e6ca35a924b4293bea0
ShinMyeongJi <shinmj1207@gmail.com>
2021-07-27 21:52:11 +0900

    210727 알고리즘

---
+++ 210727CodeWars/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+    }
+
+    public static int solution(int number) {
+        List<Integer> multiples = new ArrayList<>();
+
+        for (int i = 1; 3*i < number; i++) {
+            multiples.add(3*i);
+        }
+
+        for (int i = 1; 5*i < number; i++) {
+            multiples.add(5*i);
+        }
+
+        return multiples.stream()
+                .distinct()
+                .mapToInt(i -> i).sum();
+    }
+}
