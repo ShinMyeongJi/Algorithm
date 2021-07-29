0b149a5785e7d7804889f99067cf701fc1fa97b6
ShinMyeongJi <shinmj1207@gmail.com>
2021-07-29 23:26:20 +0900

    210720 알고리즘 //테스트만 통과 ㅠㅠ

--- 210729CodeWars/src/Main.java
+++ 210729CodeWars/src/Main.java
@@ -19,4 +19,14 @@ public class Main {
         }else return names[0] + ", " + names[1] + " and " + (names.length-2) + " others like this";
 
     }
+    //훨씬 깔끔해보인다..
+    /**
+     * switch (names.length) {
+*           case 0: return "no one likes this";
+*           case 1: return String.format("%s likes this", names[0]);
+*           case 2: return String.format("%s and %s like this", names[0], names[1]);
+*           case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+*           default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+*         }
+     */
 }
