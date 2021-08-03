d3f2734d741e42157676d3b4b2960494bf68d42f
ShinMyeongJi <shinmj1207@gmail.com>
2021-08-03 23:34:26 +0900

    210803 알고리즘.. 생각 중

---
+++ 210803CodeWars/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 210803 CodeWars - Tank Truck
+ * 1. 원기둥 높이 구하기
+ * 2. 삼각형의 높이 구하기
+ * 3. 삼각형의 밑변 구하기
+ * 4. 삼각기둥의 부피 구하기
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int tankVol(int h, int d, int vt) {
+        // your code
+        return 0;
+    }
+}
