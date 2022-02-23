59c4a5d5a91c01d1c612b2626ab34125cba8ff22
ShinMyeongJi <shinmj1207@gmail.com>
2022-02-23 21:50:53 +0900

    220222 알고리즘 수정

--- BOJ1012/src/Main.java
+++ BOJ1012/src/Main.java
@@ -6,24 +6,27 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int M, N;
     public static void main(String[] args) throws IOException {
         int reCnt = 0;
-        int M, N, K;
+        int K;
 
         int[] dy = {1, -1, 0, 0};
         int[] dx = {0, 0, 1, -1};
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for(int r = 0; r < reCnt; r++) {
+        reCnt = Integer.parseInt(br.readLine());
 
-            reCnt = Integer.parseInt(br.readLine());
+        for(int r = 0; r < reCnt; r++) {
+            int cntBug = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            M = Integer.parseInt(st.nextToken()); // 가로
-            N = Integer.parseInt(st.nextToken()); // 세로
+            M = Integer.parseInt(st.nextToken()); // 가로 col
+            N = Integer.parseInt(st.nextToken()); // 세로 row
             K = Integer.parseInt(st.nextToken());
 
             int[][] field = new int[N][M];
+            boolean[][] visited = new boolean[N][M];
 
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -37,26 +40,38 @@ public class Main {
 
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < M; k++) {
-                    if (field[j][k] == 1) {
+                    if (field[j][k] == 1 && !visited[j][k]) {
                         q.offer(new int[]{j, k});
 
                         while(!q.isEmpty()) {
                             int[] started = q.poll();
 
                             for (int l = 0; l < 4; l++) {
-                                int nextRow = started[0];
-                                int nextCol = started[1];
+                                int nextRow = started[0] + dy[l];
+                                int nextCol = started[1] + dx[l];
 
                                 //bfs
+                                if(isRange(nextRow, nextCol) && field[nextRow][nextCol] == 1 && !visited[nextRow][nextCol]) {
+                                    visited[nextRow][nextCol] = true;
+                                    q.offer(new int[]{nextRow, nextCol});
+                                }
+
                             }
 
                         }
+                        cntBug += 1;
                     }
+
                 }
             }
-
-
-
+            System.out.println(cntBug);
         }
+    }
+
+    public static boolean isRange(int row, int col) {
+        if (row >= 0 && row < N && col >=0 && col < M) {
+            return true;
+        }
+        return false;
     }
 }
