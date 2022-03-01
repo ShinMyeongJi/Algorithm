132d400bfa4d0b97657ea38bd2d35ae82be41bcf
ShinMyeongJi <shinmj1207@gmail.com>
2022-03-02 00:23:49 +0900

    220301 알고리즘 way1. 아기상어 위치부터 시작 way2. 0인 위치 부터 아기상어 위치의 거리 구하기(to-do)

--- BOJ17086/src/Main.java
+++ BOJ17086/src/Main.java
@@ -13,6 +13,10 @@ public class Main {
 
     static int[] dy = {1, -1, 1, -1, 1, -1, 0, 0}; // 세로
     static int[] dx = {-1, 1, 1, -1, 0, 0, 1, -1}; // 가로
+
+
+    static Queue<int[]> q = new LinkedList<>();
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -21,37 +25,69 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         space = new int[N][M];
+        visited = new boolean[N][M];
 
+        // way 1
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 space[i][j] = Integer.parseInt(st.nextToken());
+                if (space[i][j] == 1) q.offer(new int[]{i, j});
             }
         }
 
-       for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (space[i][j] == 0) {
-                    space[i][j] = bfs(i, j);
+        bfs();
+
+        int res = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+           for (int j = 0; j < M; j++) {
+               res = Math.max(res, space[i][j] - 1);
+           }
+        }
+
+        System.out.println(res);
+    }
+
+    public static void bfs() {
+        while (!q.isEmpty()){
+            int[] temp = q.poll();
+
+            for (int k = 0; k < 8; k++) {
+                int nextI = temp[0] + dy[k];
+                int nextJ = temp[1] + dx[k];
+
+                if (nextI < N && nextI >= 0 && nextJ < M && nextJ >= 0) {
+                    if (!visited[nextI][nextJ]){
+                        if (space[nextI][nextJ] == 1) continue;
+
+                        q.offer(new int[]{nextI, nextJ});
+                        visited[nextI][nextJ] = true;
+                        space[nextI][nextJ] = space[temp[0]][temp[1]] + 1;
+                    }
                 }
             }
         }
-
-       for (int i = 0; i < N; i++) {
-           for (int j = 0; j < M; j++) {
-               System.out.print(space[i][j] + " ");
-           }
-           System.out.println();
-       }
-
     }
+}
 
-    public static int bfs(int i, int j) {
+
+/* for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (space[i][j] == 0) {
+                    ds[i][j] = bfs(i, j);
+                }
+            }
+        }
+*/
+
+    /*public static int bfs(int i, int j) {
         boolean[][] visited = new boolean[N][M];
         int cnt = 0;
 
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{i, j});
+        visited[i][j] = true;
 
         while(!q.isEmpty()) {
             for(int d = 0; d < q.size(); d++) {
@@ -79,5 +115,4 @@ public class Main {
 
         return cnt;
 
-    }
-}
+    }*/
