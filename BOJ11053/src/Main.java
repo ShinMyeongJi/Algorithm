29b93f940b46d160bbdd1fa269acce78cef7dba2
ShinMyeongJi <shinmj1207@gmail.com>
2022-03-09 01:07:34 +0900

    220308 알고리즘

--- BOJ11053/src/Main.java
+++ BOJ11053/src/Main.java
@@ -3,22 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//LIS 이
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
         int A;
         int[] seq;
-
+        int[] length;
+        int res = Integer.MIN_VALUE;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         A = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         seq = new int[A];
+        length = new int[A];
 
         for (int i = 0; i < A; i++) {
             seq[i] = Integer.parseInt(st.nextToken());
         }
 
+
+        for (int i = 0; i < A; i++) {
+            length[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (seq[j] < seq[i]) {
+                    length[i] = Math.max(length[i], length[j] + 1);
+                }
+            }
+            res = Math.max(length[i], res);
+        }
+
+        System.out.println(res);
     }
 }
