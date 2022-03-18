042c416b9948a70c5803dcec96293d0b5d44eac4
ShinMyeongJi <shinmj1207@gmail.com>
2022-03-18 22:21:01 +0900

    220318 알고리즘

--- BOJ1600/src/Main.java
+++ BOJ1600/src/Main.java
@@ -5,6 +5,9 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 문제를 잘못 이해해서 말의 움직임의 좌표가 정해져있는지 몰랐다..;
+ */
 public class Main {
 
     static int K;
@@ -14,8 +17,11 @@ public class Main {
     static int[][] board;
     static int[][][] visited;
 
-    static int[] dy = {1, -1, 0, 0};
+    static int[] dy = {1, -1, 0, 0}; // 일반 사방탐색
     static int[] dx = {0, 0, 1, -1};
+
+    static int[] horseY = {-2, -1, 2, 1, -2, -1, 2, 1}; // 말의 움직임
+    static int[] horseX = {-1, -2, -1, -2, 1, 2, 1, 2};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,10 +30,11 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        H = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
         board = new int[H][W];
+        visited = new int[H][W][K + 1];
 
         for (int i = 0; i < H; i++) {
             st = new StringTokenizer(br.readLine());
@@ -36,32 +43,57 @@ public class Main {
             }
         }
 
-        q.offer(new int[]{0, 0});
+        q.offer(new int[]{0, 0, 0});
+
+        System.out.println(bfs());
     }
 
     public static int bfs() {
         while (!q.isEmpty()) {
             int[] temp = q.poll();
 
+            int curY = temp[0];
+            int curX = temp[1];
+            int curK = temp[2];
+
+            if(curY == H - 1 && curX == W - 1) {
+                return visited[curY][curX][curK];
+            }
             for (int i = 0; i < 4; i++) {
-                int nextY = temp[0] + dy[i];
-                int nextX = temp[1] + dx[i];
+                int nextY = curY + dy[i];
+                int nextX = curX + dx[i];
 
-                int move = temp[2];
-
-                if (isRange(nextY, nextX)) {
-                    if (board[nextY][nextX] == 0) {
-
+                if (isRange(nextY, nextX, curK)) {
+                    if(visited[nextY][nextX][curK] == 0){
+                        visited[nextY][nextX][curK] = visited[curY][curX][curK] + 1;
+                        q.offer(new int[]{nextY, nextX, curK});
                     }
                 }
             }
 
+            if (curK < K) {     // 아직 말처럼 뛸 수 있다면
+                for (int i = 0; i < 8; i++) {
+                    int nextY = curY + horseY[i];
+                    int nextX = curX + horseX[i];
+
+                    if (isRange(nextY, nextX, curK)) {
+                        if(visited[nextY][nextX][curK + 1] == 0) { // 말처럼 뛰었을 때의 루트를 체크한다. (아직 방문하지 않았는지)
+                            visited[nextY][nextX][curK + 1] = visited[curY][curX][curK] + 1; //
+                            q.offer(new int[]{nextY, nextX, curK + 1});
+                        }
+                    }
+                }
+            }
         }
         return -1;
     }
 
-    public static boolean isRange(int y, int x) {
-        if (y >= 0 && y < H && x >= 0 && x < W) return true;
+    public static boolean isRange(int y, int x, int k) {
+        if (y >= 0 && y < H && x >= 0 && x < W)  {
+            if (board[y][x] == 0) {
+                return true;
+            }
+        }
         return false;
     }
 }
