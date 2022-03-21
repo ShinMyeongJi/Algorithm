c12a0ab2f1090b181e0c088af3150bfd4e763f95
ShinMyeongJi <shinmj1207@gmail.com>
2022-03-21 21:10:46 +0900

    220321 알고리즘

---
+++ BOJ16953/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 범위...!!!!! 제발 범위..!!!!!!1
+ */
+public class Main {
+
+    static long A,B;
+    static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+
+        dfs(A, 1);
+        if (min == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(min);
+    }
+
+    public static void dfs(long res, int cnt) {
+        if (res == B) {
+            min = Math.min(min, cnt);
+            return;
+        }else if (res > B) {
+            return;
+        }
+
+
+        dfs(res * 2, cnt + 1);
+        dfs(res * 10 + 1, cnt + 1);
+    }
+}
