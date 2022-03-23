9f27ac4ce2d757490e2fb55c9de093a0040ed223
ShinMyeongJi <shinmj1207@gmail.com>
2022-03-23 23:51:05 +0900

    220323 알고리즘

--- BOJ7569/src/Main.java
+++ BOJ7569/src/Main.java
@@ -1,12 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M, H;
     static int[][][] boxes;
+    static Queue<int[]> q = new LinkedList<>();
+
+    static int[] dy = {-1, 0, 1, 0, 0, 0};
+    static int[] dx = {0, 1, 0, -1, 0, 0};
+    static int[] dh = {0, 0, 0, 0, 1, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,6 +23,17 @@ public class Main {
         H = Integer.parseInt(st.nextToken());
 
         boxes = new int[N][M][H];
+
+        for(int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < M; k++) {
+                    boxes[j][k][i] = Integer.parseInt(st.nextToken());
+                    if (boxes[j][k][i] == 1) q.offer(new int[]{j, k, i});
+                }
+             }
+        }
+
 
 
     }
