7f8733f406f630baf29325fef0f4803d2010d039
ShinMyeongJi <shinmj1207@gmail.com>
2022-03-27 22:46:42 +0900

    220327 알고리즘
    일반 HashMap으로 하는게 훨씬 효율적..

--- BOJ14425/src/Main.java
+++ BOJ14425/src/Main.java
@@ -3,13 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * Main에서 각 Node에 접근하면 시간초과가 남..,,
+ * Main에 직접 작성할 시 가능..
+ */
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        Map<String, Integer> map = new HashMap<>();
+        int cnt = 0;
+        TrieNode rootNode = new TrieNode();
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            rootNode.insert(str);
+        }
+
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+
+            if (rootNode.contains(str)) cnt++;
+        }
+
+        System.out.println(cnt);
+
+        /*Map<String, Integer> map = new HashMap<>();
         int cnt = 0;
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
@@ -21,9 +44,8 @@ public class Main {
         for (int i = 0; i < M; i++) {
             String str = br.readLine();
             if (map.get(str) != null) cnt++;
-        }
+        }*/
 
-        System.out.println(cnt);
     }
 }
 
@@ -32,7 +54,33 @@ class TrieNode {
     boolean isLastChar;
 
     public void insert(String word) {
+        TrieNode trieNode = this;
 
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            trieNode.childNode.computeIfAbsent(c, s -> new TrieNode());
+            trieNode = trieNode.childNode.get(c);
+
+            if(i == word.length() - 1) {
+                trieNode.isLastChar = true;
+                return;
+            }
+        }
+    }
+
+    public boolean contains(String word) {
+        TrieNode trieNode = this;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            TrieNode node = trieNode.childNode.get(c);
+            if (node == null) return false;
+            trieNode = node;
+        }
+
+        return trieNode.isLastChar;
     }
 }
 
