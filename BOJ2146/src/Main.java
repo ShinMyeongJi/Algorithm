2fccd1b3ca8371252ad475e73abdbd6dbe1c7aff
ShinMyeongJi <shinmj1207@gmail.com>
2022-04-17 16:30:37 +0900

    220417 알고리즘 뭐가 잘못된건 모르겠지만 디버깅만 5번을 했는데 모르겠어서 여러 번 재시도 한 문제,,,,, 꼭 다시 풀어볼 것...

--- BOJ2146/src/Main.java
+++ BOJ2146/src/Main.java
@@ -11,11 +11,11 @@ public class Main {
 
     static Queue<int[]> q = new LinkedList<>();
 
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
 
     static boolean[][] visited;
-    static int label = 1;
+    static int num = 2;
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
@@ -33,64 +33,37 @@ public class Main {
             }
         }
 
-        // 각 섬의 다른 Label 부여하기
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if(!visited[i][j] && country[i][j] == 1){
-                    q.offer(new int[]{i, j});
-                    visited[i][j] = true;
-                    label++;
-                    country[i][j] = label;
-                    changeAreaLabel();
+                if (!visited[i][j] && country[i][j] != 0){
+                    islandNumbering(i, j);
+                    num++;
                 }
             }
         }
 
+        visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (country[i][j] != 0) {
+            for (int j =0; j < N; j++) {
+                if (!visited[i][j] && country[i][j] != 0) {
+                    bfs(i, j, country[i][j]);
                     visited = new boolean[N][N];
-                    q = new LinkedList<>();
-                    q.offer(new int[]{i, j, 0});
-                    visited[i][j] = true;
-
-                    int res = bfs(i, j);
-
-                    if (res >= 0) min = Math.min(min, res);
                 }
             }
         }
+
 
         System.out.println(min);
     }
 
-    public static int bfs(int y, int x) {
 
+    public static void islandNumbering(int y, int x) {
+        Queue<int[]> q = new LinkedList<>();
 
-        int curLabel = country[y][x];
+        visited[y][x] = true;
+        q.offer(new int[]{y, x});
+        country[y][x] = num;
 
-        while(!q.isEmpty()) {
-            int[] temp = q.poll();
-            int cur = country[temp[0]][temp[1]];
-
-            if (cur != 0 && cur != curLabel) return temp[2];
-
-            for(int i = 0; i < 4; i++) {
-                int nextY = temp[0] + dy[i];
-                int nextX = temp[1] + dx[i];
-                int cnt = temp[2];
-
-                if (isRange(nextY, nextX) && !visited[nextY][nextX] && country[nextY][nextX] != cur) {
-                    visited[nextY][nextX] = true;
-                    q.offer(new int[]{nextY, nextX, cnt + 1});
-                }
-            }
-        }
-
-        return -1;
-    }
-
-    public static void changeAreaLabel() {
         while (!q.isEmpty()) {
             int[] temp = q.poll();
 
@@ -98,10 +71,40 @@ public class Main {
                 int nextY = temp[0] + dy[i];
                 int nextX = temp[1] + dx[i];
 
-                if (isRange(nextY, nextX) && country[nextY][nextX] == 1 && !visited[nextY][nextX]) {
-                    country[nextY][nextX] = label;
+                if (isRange(nextY, nextX) && !visited[nextY][nextX] && country[nextY][nextX] == 1) {
                     visited[nextY][nextX] = true;
+                    country[nextY][nextX] = num;
                     q.offer(new int[]{nextY, nextX});
+                }
+            }
+        }
+    }
+
+    public static void bfs(int y, int x, int curIslandNum) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{y, x, 0});
+        visited[y][x] = true;
+
+        while(!q.isEmpty()) {
+            int[] temp = q.poll();
+
+            int curY = temp[0];
+            int curX = temp[1];
+            int curCnt = temp[2];
+
+            for (int i = 0;i < 4; i++) {
+                int nextY = curY + dy[i];
+                int nextX = curX + dx[i];
+
+                if (isRange(nextY, nextX) && !visited[nextY][nextX]) {
+                    if (country[nextY][nextX] == 0) {
+                        q.offer(new int[]{nextY, nextX, curCnt + 1});
+                        visited[nextY][nextX] = true;
+                    }else if(country[nextY][nextX] != 0 && country[nextY][nextX] != curIslandNum){
+                        min = Math.min(min, curCnt);
+                        q.clear();
+                        return;
+                    }
                 }
             }
         }
