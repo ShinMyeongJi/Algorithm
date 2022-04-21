693847c0c80368478e86369f01f211710f7dc3c6
ShinMyeongJi <shinmj1207@gmail.com>
2022-04-21 22:52:14 +0900

    220421 알고리즘 - ??????????/

--- BOJ2343/src/Main.java
+++ BOJ2343/src/Main.java
@@ -1,13 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
-/**
- * 이진탐색의 경 무엇을 탐색하는지를 생각하고 이를 탐색 대상으로 계산할 수 있게 식을 유도하는 것이 옳은 해법이다.
- */
 public class Main {
     public static void main(String[] args) throws IOException {
         int N, M = 0;
@@ -20,36 +15,67 @@ public class Main {
         int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
         int low = -1;
+        int high = 0;
         for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             low = Math.max(low, arr[i]);
+            high += arr[i];
         }
 
-        int high = IntStream.of(arr).sum();
+        int res = Integer.MAX_VALUE;
 
-
-        while(low <= high) {
+        while (low <= high) {
             int mid = (low + high) / 2;
             int cnt = 0;
-            int sum = 0;
-            for(int i = 0; i < N; i++) {
-                if(sum + arr[i] > mid) { // 초과되면 딱 그 직전까지만 더한다.
+
+            int remainLength = mid;
+
+            for (int i = 0; i < N; i++) {
+                if (remainLength - arr[i] < 0) {
                     cnt++;
-                    sum = 0;
+                    remainLength = mid;
                 }
-                sum += arr[i];
+                remainLength -= arr[i];
             }
 
-            if(sum != 0) cnt++;
+            if (remainLength > 0) cnt++;
 
-            if(cnt <= M) {
+            if (cnt <= M) {
                 high = mid - 1;
-            }else{
+            }else {
                 low = mid + 1;
             }
-
         }
 
         System.out.println(low);
     }
 }
+
+
+
+        /*int high = IntStream.of(arr).sum();
+
+
+        while(low <= high) {
+                int mid = (low + high) / 2;
+                int cnt = 0;
+                int sum = 0;
+                for(int i = 0; i < N; i++) {
+        if(sum + arr[i] > mid) { // 초과되면 딱 그 직전까지만 더한다.
+        cnt++;
+        sum = 0;
+        }
+        sum += arr[i];
+        }
+
+        if(sum != 0) cnt++;
+
+        if(cnt <= M) {
+        high = mid - 1;
+        }else{
+        low = mid + 1;
+        }
+
+        }
+
+        System.out.println(low);*/
