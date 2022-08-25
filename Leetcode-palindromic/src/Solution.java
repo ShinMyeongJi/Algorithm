775a6932357d208bd5b41804b5ae895a9d5d6090
ShinMyeongJi <shinmj1207@gmail.com>
2022-08-25 23:01:45 +0900

    220825 longest palindromic

---
+++ Leetcode-palindromic/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+
+    int min = 2;
+    int max;
+
+    Stack<String> paldrmicMachine = new Stack<>();
+
+    public String longestPalindrome(String s) {
+
+        max = s.length();
+
+
+        for (int i = 1; i < s.length(); i++) {
+
+        }
+    }
+
+
+}
